3f2c41173ca356fd676f7642bbf66964779fdccb
Tarinda Wickramasinghe <tarinda.wickramasinghe@iag.com.au>
2021-06-22 10:33:17 +1000

    Initial Commit

---
+++ AutoLayoutTest3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayoutTest3
+//
+//  Created by Tarinda on 19/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
